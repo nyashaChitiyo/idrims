5e2e97d9dd0b865a365d561652da176a164bbf5b
unknown <rudomoyo59@gmail.com>
2018-09-12 11:45:15 +0200

    rudo 12/9/18- 1145

--- idrims/src/app/main/view-monthly-report/view-monthly-report.component.ts
+++ idrims/src/app/main/view-monthly-report/view-monthly-report.component.ts
@@ -9,18 +9,20 @@ import {Router} from '@angular/router';
   styleUrls: ['./view-monthly-report.component.css']
 })
 export class ViewMonthlyReportComponent implements OnInit {
+
   dtOptions: DataTables.Settings = {};
   public monthly= [];
   public temp_var: Object = false;
 
   colpointID : string;
+  userid : string;
 
   constructor( private demo: DemoService,private router: Router) { 
     this.getAll();
   }
 
   ngOnInit() {
-
+ 
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 5
@@ -44,6 +46,21 @@ export class ViewMonthlyReportComponent implements OnInit {
     //console.log(userId+'user id is this')
     this.demo.post('http://108.61.174.41:7070/api/reports/view/all/month/collectionPointId',{
       "id": this.colpointID
+    })
+    .subscribe(
+      (data: Response)=> {
+        let arr = [];
+        arr.push(data)
+        this.monthly = arr[0];
+        console.log(this.monthly);
+        this.temp_var=true;
+      }
+    ) 
+  }
+
+  getByUserID(){
+    this.demo.post('http://108.61.174.41:7070/api/reports/view/all/month/userId',{
+      "id": this.userid
     })
     .subscribe(
       (data: Response)=> {
